2cd99d6b370698d9bacbfb8e430a394170402a1a
Chris Davis <chris.davis5440@gmail.com>
2016-08-05 13:43:59 -0400

    Created first backbone model

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
-var Person = function(config) {
-	this.name = config.name;
-	this.age = config.age;
-	this.occupation = config.occupation;
-};
-
-Person.prototype.work = function() {
-	return this.name + " is working.";
-};
-
-var person1 = new Person({
-	name: "Chris Davis",
-	age: 40,
-	occupation: "Software Developer"
-})
+var Person = Backbone.Model.extend({
+	defaults: {
+		name: 'Guest User',
+		age: 23,
+		occupation: 'Worker'
+	},
+	work: function(){
+		return this.get('name')  +  'is working.';
+	}
+});
