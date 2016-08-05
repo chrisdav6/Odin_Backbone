d066c1a2afb171d48ca69509e30cbaaa63df18c4
Chris Davis <chris.davis5440@gmail.com>
2016-08-05 14:02:30 -0400

    Added Validation

--- js/main.js
+++ js/main.js
@@ -2,9 +2,20 @@ var Person = Backbone.Model.extend({
 	defaults: {
 		name: 'Guest User',
 		age: 23,
-		occupation: 'Worker'
+		occupation: 'worker'
 	},
+
+	validate: function(attributes){
+		if ( attributes.age <  0){
+			return 'Age must be positive.';
+		}
+
+		if ( !attributes.name ){
+			return 'Every person must have a name.';
+		}
+	},
+
 	work: function(){
-		return this.get('name')  +  'is working.';
+		return this.get('name') + ' is working.';
 	}
 });
